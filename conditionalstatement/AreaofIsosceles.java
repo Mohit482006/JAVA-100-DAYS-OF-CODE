7758e11406b58156cf7a0bfc6e6c89a22cab3b4d
Mohit482006 <mohitsinghbha9399@gmail.com>
2025-07-13 01:47:06 +0000

    practice

--- conditionalstatement/AreaofIsosceles.java
+++ conditionalstatement/AreaofIsosceles.java
@@ -11,5 +11,7 @@ public class AreaofIsosceles {
         System.out.println("Enter the value of height of triangle");
         int height=input.nextInt();
 
+        System.out.println("Area of isosceles triamgle is "+(0.5*base*(Math.sqrt(Math.pow(height, 2)-((Math.pow(base, 2)/4))))));
+
     }
 }
