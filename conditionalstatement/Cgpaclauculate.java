4998a463e0b59953fad2e134fe540b77d122e5df
Mohit482006 <mohitsinghbha9399@gmail.com>
2025-07-15 01:30:40 +0000

    scanner close

--- conditionalstatement/Cgpaclauculate.java
+++ conditionalstatement/Cgpaclauculate.java
@@ -3,7 +3,26 @@ package conditionalstatement;
 import java.util.Scanner;
 
 public class Cgpaclauculate {
-    Scanner scanner=new Scanner(System.in);
+    public static void main(String[] args){
+        Scanner scanner= new Scanner(System.in);
 
-    
+        System.out.println("Enter the total number of subjects :");
+        int totalno=scanner.nextInt();
+
+        System.out.println("Enter the gradepoints");
+        int sum=0;
+        for(int i=1;i<=totalno;i++){
+            int gradepoints=scanner.nextInt();
+            System.out.println("Subject "+i+":"+" "+ gradepoints);
+            sum+=gradepoints;
+        }
+        System.out.println("Sum of gradepoints is :"+sum);
+
+        double cgpa= sum/totalno;
+
+        System.out.println("It is CGPA :"+cgpa);
+
+        scanner.close();
+
+    }    
 }
