c6d6a8824d03e1f212a8a5af0d5e1d9a9cc8f466
Mohit482006 <mohitsinghbha9399@gmail.com>
2025-07-18 11:49:41 +0000

    scanner close

--- function/primeornot.java
+++ function/primeornot.java
@@ -21,7 +21,7 @@ public class primeornot {
         if(x<2){
             return false;
         }
-        for(int i=2;i<=x;i++){
+        for(int i=2;i<x;i++){
             if(x%i==0){
                 return false;
             }
