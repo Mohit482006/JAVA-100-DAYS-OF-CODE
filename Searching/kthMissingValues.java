8d13aa7356cd549b51e5ab9037a17a7c7aed4f06
Mohit482006 <mohitsinghbha9399@gmail.com>
2025-09-22 22:18:32 +0530

    leetcode queno.1539

---
+++ Searching/kthMissingValues.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int findKthPositive(int[] arr, int k) {
+        int left = 0, right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            // Numbers missing until arr[mid]
+            int missing = arr[mid] - (mid + 1);
+
+            if (missing < k) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        // The kth missing number is k + left
+        return left + k;
+    }
+}
