0118bedde3961558a22dc3cf8fd183e6e4a555d3
Mohit482006 <mohitsinghbha9399@gmail.com>
2026-02-04 12:14:15 +0530

    Delete Sorting/Maximum_product_of_two_elements.java

--- Sorting/Maximum_product_of_two_elements.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public int maxProduct(int[] nums) {
-        int n=nums.length;
-        Arrays.sort(nums);
-        int res;
-        res=(nums[n-1]-1)*(nums[n-2]-1);
-        return res;
-    }
-}
