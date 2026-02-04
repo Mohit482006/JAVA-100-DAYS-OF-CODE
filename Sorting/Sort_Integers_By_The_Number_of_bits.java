86d4d457040fa18650dc3de3c8abb5e55810b8d3
Mohit482006 <mohitsinghbha9399@gmail.com>
2026-02-04 11:32:10 +0530

    leetcode queno.1356

--- Sorting/Sort_Integers_By_The_Number_of_bits.java
+++ Sorting/Sort_Integers_By_The_Number_of_bits.java
@@ -1,28 +1,28 @@
+import java.util.*;
 
 class Solution {
-    public int[] sortByBits(int[] arr) 
-    {
-        Integer temp [] = new Integer[arr.length];
+    public int[] sortByBits(int[] arr) {
 
-        for(int i=0;i<arr.length; i++)
-        {
-            temp[i]  = arr[i];
+        Integer[] temp = new Integer[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            temp[i] = arr[i];
         }
 
-        Arrays.sort(temp, (a, b)->{
-                int bitsA = Integer.bitCount(a);
-                int bitsB = Integer.bitCount(b);
-                int c = (bitsA-bitsB);
-                c = (c!=0)?c:(a-b);
-                return c;
+        Arrays.sort(temp, (a, b) -> {
+            int bitsA = Integer.bitCount(a);
+            int bitsB = Integer.bitCount(b);
+
+            if (bitsA != bitsB) {
+                return bitsA - bitsB;   // sort by number of 1s
             }
-        );
-        for(int i=0;i<temp.length; i++)
-        {
-            arr[i]  = temp[i];
+            return a - b;               // tie-break by value
+        });
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = temp[i];
         }
+
         return arr;
-        
-        
     }
 }
+
